fb832e7c2cff86cb6230827d047f674ca2091893
KitapakMatra <160598991+KitapakMatra@users.noreply.github.com>
2024-02-20 18:16:53 +0700

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const { isMobile } = useDevice()
+if (isMobile)
+  configStore.appContentLayoutNav = 'vertical'
+</script>
+
+<template>
+  <VApp>
+    <NuxtLayout>
+      <title>My page title</title>
+      <NuxtPage />
+    </NuxtLayout>
+  </VApp>
+</template>
